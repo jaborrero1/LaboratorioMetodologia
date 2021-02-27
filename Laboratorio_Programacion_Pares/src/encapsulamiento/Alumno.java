72fe307d5a5498ca27361bb7e61fb0fa5dae8d6c
jaborrero1 <jorgerey2011@gmail.com>
2021-02-26 22:30:35 -0500

    Version continua 1

---
+++ Laboratorio_Programacion_Pares/src/encapsulamiento/Alumno.java
@@ -0,0 +1,38 @@
+package encapsulamiento;
+
+/*
+ * Jorge Borrero
+ */
+
+public class Alumno {
+
+	private int noControl;
+	private String email;
+	public String nombre;
+	public String apellidos;
+	
+	
+	void setNoControl(int NC) {
+		this.noControl = NC;
+	}
+	
+	int getNoControl() {
+		return noControl;
+	}
+	
+	void setEmail(String EM) {
+		this.email = EM;
+	}
+	
+	String getEmail() {
+		return email;
+	}
+	
+	void verDatos(){
+		System.out.println("No. de Control: " + noControl);
+		System.out.println("Nombre: " + nombre);
+		System.out.println("Apellidos: " + apellidos);
+		System.out.println("Email: " + email);
+	}
+	
+}
---
+++ Laboratorio_Programacion_Pares/src/encapsulamiento/Encapsulamiento.java
@@ -0,0 +1,33 @@
+package encapsulamiento;
+
+/*
+ * Jorge Borrero
+ */
+
+public class Encapsulamiento {
+	
+public static void main(String[] args) {
+		
+	Alumno alum1 = new Alumno();
+	Alumno alum2 = new Alumno();
+	
+	alum1.setNoControl(65134);
+	alum1.nombre = "Jorge";
+	alum1.apellidos = "Borrero";
+	alum1.setEmail("jborrero@gmail.com");
+	alum1.verDatos();
+	System.out.println("Accediendo al No. de control " + 
+			alum1.getNoControl()+ " Que es privado");
+	
+	System.out.println();
+	
+	alum2.setNoControl(54512);
+	alum2.nombre = "Nicole";
+	alum2.apellidos = "Andrade";
+	alum2.setEmail("nandrade@gmail.com");
+	alum2.verDatos();
+	System.out.println("Accediendo al No. de control " + 
+			alum2.getNoControl()+ " Que es privado");
+	
+	}
+}
